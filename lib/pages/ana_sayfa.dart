cd616d5334b97166d21f5681d76230394b1ad98d
Mahir Yapar <mahiryapar2453@gmail.com>
2025-03-04 16:27:44 +0300

    Shared Preferences hatası giderildi. Sayfalar için dosyalar açıldı giriş yapıldı.

---
+++ lib/pages/ana_sayfa.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class AnaSayfa extends StatefulWidget {
+  @override
+  _AnaSayfaState createState() => _AnaSayfaState();
+}
+
+class _AnaSayfaState extends State<AnaSayfa> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ana Sayfa'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.notifications),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text(
+          'Seçili Sayfa: $_selectedIndex',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Ana Sayfa'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Keşfet'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profil'),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
